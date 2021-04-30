0b53276b24a443327d567534166668407f3a51ae
Ryan Chipman <ryan@ryanchipman.com>
2021-04-30 17:39:11 -0400

    BI-2631: Update visual studio versions in platforms.sh

--- bld/bin/platforms.sh
+++ bld/bin/platforms.sh
@@ -49,8 +49,8 @@ win32)
 	# Use OpenSSL on Windows.
     CMAKE_ARGS="$CMAKE_ARGS -DWITH_SSL=C:/openssl32/openssl-1_0_2k"
     CMAKE_PATH='/cygdrive/c/cmake/bin'
-    CMAKE_GENERATOR='Visual Studio 14 2015'
-    DEVENV_PATH='/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE'
+    CMAKE_GENERATOR='Visual Studio 16 2019'
+    DEVENV_PATH='/cygdrive/c/Program Files (x86)/Microsoft Visual Studio/2019/Professional/Common7/IDE'
     BISON_PATH='/cygdrive/c/bison/bin'
     ICU_PLATFORM='Cygwin/MSVC'
     VARIANT='windows-32'
@@ -60,9 +60,9 @@ win64)
     PLATFORM_NAME='windows'
 	# Use OpenSSL on Windows.
     CMAKE_ARGS="$CMAKE_ARGS -DWITH_SSL=C:/openssl"
-    CMAKE_GENERATOR='Visual Studio 14 2015 Win64'
+    CMAKE_GENERATOR='Visual Studio 16 2019 Win64'
     CMAKE_PATH='/cygdrive/c/cmake/bin'
-    DEVENV_PATH='/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE'
+    DEVENV_PATH='/cygdrive/c/Program Files (x86)/Microsoft Visual Studio/2019/Professional/Common7/IDE'
     BISON_PATH='/cygdrive/c/bison/bin'
     ICU_PLATFORM='Cygwin/MSVC'
     VARIANT='windows-64'
