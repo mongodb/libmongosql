31552240523dffee02e0278a2893528aae72eb9d
Ryan Chipman <ryan@ryanchipman.com>
2021-05-10 09:49:58 -0400

    BI-2631: Use newer CMake path

--- bld/bin/platforms.sh
+++ bld/bin/platforms.sh
@@ -61,7 +61,7 @@ win64)
 	# Use OpenSSL on Windows.
     CMAKE_ARGS="$CMAKE_ARGS -DWITH_SSL=C:/openssl"
     CMAKE_GENERATOR='Visual Studio 16 2019 Win64'
-    CMAKE_PATH='/cygdrive/c/cmake/bin'
+    CMAKE_PATH='/cygdrive/c/Program Files/CMake/bin'
     DEVENV_PATH='/cygdrive/c/Program Files (x86)/Microsoft Visual Studio/2019/Professional/Common7/IDE'
     BISON_PATH='/cygdrive/c/bison/bin'
     ICU_PLATFORM='Cygwin/MSVC'
