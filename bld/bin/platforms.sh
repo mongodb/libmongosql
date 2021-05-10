4f36f4bd1aa1ed270847fb506095673103352e9a
Ryan Chipman <ryan@ryanchipman.com>
2021-05-10 10:19:14 -0400

    fixup

--- bld/bin/platforms.sh
+++ bld/bin/platforms.sh
@@ -48,7 +48,7 @@ win32)
     PLATFORM_NAME='windows'
 	# Use OpenSSL on Windows.
     CMAKE_ARGS="$CMAKE_ARGS -DWITH_SSL=C:/openssl32/openssl-1_0_2k"
-    CMAKE_PATH='/cygdrive/c/cmake/bin'
+    CMAKE_PATH='/cygdrive/c/Program Files/CMake/bin'
     CMAKE_GENERATOR='Visual Studio 16 2019'
     DEVENV_PATH='/cygdrive/c/Program Files (x86)/Microsoft Visual Studio/2019/Professional/Common7/IDE'
     BISON_PATH='/cygdrive/c/bison/bin'
