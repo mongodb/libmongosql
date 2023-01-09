6c641a63069cac5185494dc8a2b06a661c79681b
Patrick Meredith <pmeredit@gmail.com>
2023-01-09 15:45:50 -0500

    BI-2668: Test local openssl

--- bld/bin/platforms.sh
+++ bld/bin/platforms.sh
@@ -37,15 +37,12 @@ rhel70)
 macos)
     PLATFORM_ARCH='64'
     PLATFORM_NAME='darwin'
-    SSL_DIR="$BUILD_DIR/1.0.2n"
-    mkdir -p $BUILD_DIR
-    cd $BUILD_DIR
+    SSL_DIR="$PWD/1.0.2n"
     curl -O https://mongo-bic-odbc-driver-resources.s3.amazonaws.com/macos/openssl-1.0.2n.zip
     unzip openssl-1.0.2n.zip
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     echo "$SSL_DIR"
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    cd -
     CMAKE_ARGS="$CMAKE_ARGS -DWITH_SSL=$SSL_DIR -DCMAKE_VERBOSE_MAKEFILE=ON"
     CMAKE_GENERATOR='Unix Makefiles'
     CMAKE_PATH='/Applications/Cmake.app/Contents/bin'
